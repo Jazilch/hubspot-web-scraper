d1e2a81d0055f83cfe7347542a9fdc33b05c321e
Jazilch <jzilch@hubspot.com>
2019-03-19 13:11:26 +0000

    removed /build from .gitignore

---
+++ client/build/precache-manifest.17d3d3ff16eaf2cbbbc55626d45c883e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a1b7ec7b87945a9a057e",
+    "url": "/static/js/main.a1b7ec7b.chunk.js"
+  },
+  {
+    "revision": "a1ddde5b39feda86b49b",
+    "url": "/static/js/2.a1ddde5b.chunk.js"
+  },
+  {
+    "revision": "a1b7ec7b87945a9a057e",
+    "url": "/static/css/main.678f5cc1.chunk.css"
+  },
+  {
+    "revision": "69c58cd8d3b9e515bf1f404c7a2c3526",
+    "url": "/index.html"
+  }
+];
